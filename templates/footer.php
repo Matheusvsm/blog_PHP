a57657d9a713d03e7d935135a23a55eee1c3a3e3
matheusvsm <matheusmoura.eng.eletrica@gmail.com>
2023-07-26 20:50:30 -0300

    criando primeiros templates

---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>Matheus Blog &copy; 2023</p>
+</footer>
+</body>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,25 @@
+<?php
+    include_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Software</title>
+    <!-- Estilo do Projeto -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/style.css">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <a href="<?=$BASE_URL?>" id="logo">
+                <img src="<?=$BASE_URL ?>/img/logo.svg" alt="Blog Matheus">
+            </a>
+        </header>
+    </body>
+    </html>
